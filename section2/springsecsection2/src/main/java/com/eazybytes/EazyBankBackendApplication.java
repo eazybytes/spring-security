5c4e4740331efb08463009e759320f7b3d6e6c40
eazybytes <tutor@eazybytes.com>
2024-05-26 11:21:04 +0530

    Custom Security, PermitAll, DenyAll Configurations

--- section2/springsecsection2/src/main/java/com/eazybytes/EazyBankBackendApplication.java
+++ section2/springsecsection2/src/main/java/com/eazybytes/EazyBankBackendApplication.java
@@ -2,13 +2,12 @@ package com.eazybytes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class EazyBankBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EazyBankBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EazyBankBackendApplication.class, args);
+    }
 
 }
